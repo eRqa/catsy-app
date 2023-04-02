0424b1733262d4119b37783d2af42422954719b6
eRqa <ivlasov.spb@gmail.com>
2023-04-02 15:47:18 +0300

    add fraud microservice

---
+++ fraud/src/main/java/ru.ivlasov.fraud/FraudCheckHistory.java
@@ -0,0 +1,28 @@
+package ru.ivlasov.fraud;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class FraudCheckHistory {
+    @Id
+    @SequenceGenerator(
+            name = "fraud_id_sequence",
+            sequenceName = "fraud_id_sequence")
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "fraud_id_sequence"
+    )
+    private int id;
+    private int customerId;
+    private boolean isFraudster;
+    private LocalDateTime createdAt;
+}
