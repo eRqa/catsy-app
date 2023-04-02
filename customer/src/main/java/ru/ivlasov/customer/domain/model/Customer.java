7099e3ce80d2d709aad7b416fb4da39b413aa4cf
eRqa <ivlasov.spb@gmail.com>
2023-04-02 15:14:56 +0300

    add save customer

--- customer/src/main/java/ru/ivlasov/customer/domain/model/Customer.java
+++ customer/src/main/java/ru/ivlasov/customer/domain/model/Customer.java
@@ -1,11 +1,27 @@
 package ru.ivlasov.customer.domain.model;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence")
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private int id;
+
     private String firstName;
     private String lastName;
     private String email;
--- customer/src/main/java/ru/ivlasov/customer/domain/service/CustomerService.java
+++ customer/src/main/java/ru/ivlasov/customer/domain/service/CustomerService.java
@@ -2,17 +2,18 @@ package ru.ivlasov.customer.domain.service;
 
 import org.springframework.stereotype.Service;
 import ru.ivlasov.customer.domain.model.Customer;
+import ru.ivlasov.customer.persistence.CustomerRepository;
 import ru.ivlasov.customer.view.request.CustomerRegistrationRequest;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void register(CustomerRegistrationRequest customerRegistrationRequest) {
         Customer customer = new Customer();
-        customer.setFirstName(customer.getFirstName());
-        customer.setLastName(customer.getLastName());
-        customer.setEmail(customer.getEmail());
+        customer.setFirstName(customerRegistrationRequest.firstName());
+        customer.setLastName(customerRegistrationRequest.lastName());
+        customer.setEmail(customerRegistrationRequest.email());
+        customerRepository.save(customer);
         //todo check valid email in view layer
         //todo check email is not taken
-        //todo persist customer
     }
 }
